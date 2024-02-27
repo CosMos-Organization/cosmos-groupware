414b92e8f089988a91b336f94838946603ceb493
Lee Min Jae <uiui96@naver.com>
2024-02-27 14:02:13 +0900

    feat : project init dependency : lombok spring boot devtools spring web spring security spring data jpa mysql driver

---
+++ Back-end/cosmos/src/test/java/com/nklcb/cosmos/CosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nklcb.cosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
