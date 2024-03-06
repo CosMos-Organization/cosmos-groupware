06d2e2ef4eec20c12970da4f24b48562907e8a71
Lee Min Jae <uiui96@naver.com>
2024-03-06 10:32:08 +0900

    #4 feat : member entity 추가

--- Back-end/cosmos/src/main/java/com/nklcb/cosmos/member/entity/Member.java
+++ Back-end/cosmos/src/main/java/com/nklcb/cosmos/member/entity/Member.java
@@ -1,4 +1,24 @@
 package com.nklcb.cosmos.member.entity;
 
-public class Member {
+import com.nklcb.cosmos.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String memberPk;
+
+    @Column(length = 255, nullable = false)
+    private String memberId;
+
+    @Column(length = 255, nullable = false)
+    private String memberPassword;
+
+    @Column(length = 255, nullable = false)
+    private String name;
 }
