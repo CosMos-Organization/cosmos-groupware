928b2f6dfe6b46e93011eb49801d9992fe32e102
philosokey-M <wlghd99589@gmail.com>
2024-03-10 22:25:57 +0900

    fix : Add email write API
    이메일 작성 서비스 코드추가
    
    미완성(repo 부분 미포함)

--- Back-end/cosmos/src/main/java/com/nklcb/cosmos/email/repository/EmailRepository.java
+++ Back-end/cosmos/src/main/java/com/nklcb/cosmos/email/repository/EmailRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmailRepository extends JpaRepository {
 
-
+    //int save()
 
 }
--- Back-end/cosmos/src/main/java/com/nklcb/cosmos/email/service/EmailService.java
+++ Back-end/cosmos/src/main/java/com/nklcb/cosmos/email/service/EmailService.java
@@ -1,15 +1,24 @@
 package com.nklcb.cosmos.email.service;
 
 import com.nklcb.cosmos.email.dto.EmailDto;
+import com.nklcb.cosmos.email.repository.EmailRepository;
+import lombok.RequiredArgsConstructor;
 import nonapi.io.github.classgraph.json.JSONUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class EmailService {
 
 
+    private final EmailRepository emailRepository;
+
+
     public String writeEmail(EmailDto emailDto) {
 
+
+        emailRepository.save(emailDto);
 
         return null;
 
