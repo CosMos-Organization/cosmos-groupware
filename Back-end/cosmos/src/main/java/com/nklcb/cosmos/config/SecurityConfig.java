c84795c00729ed28d32bddff0117399966cd7887
ldy28 <108062744+ldy28@users.noreply.github.com>
2024-03-10 15:47:16 +0900

    #25 spring security 설정

--- Back-end/cosmos/src/main/java/com/nklcb/cosmos/config/SecurityConfig.java
+++ Back-end/cosmos/src/main/java/com/nklcb/cosmos/config/SecurityConfig.java
@@ -1,5 +1,67 @@
 package com.nklcb.cosmos.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // CORS 설정
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+            config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000")); //
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequest ->
+                        authorizeRequest
+                                .requestMatchers("/error").permitAll()
+                                .requestMatchers("/swagger.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/schedule/**").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+//                .headers(
+//                        headersConfigurer ->
+//                                headersConfigurer
+//                                        .frameOptions(
+//                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
+//                                        )
+//                );
+
+        return http.build();
+    }
 
 }
--- Back-end/cosmos/src/main/java/com/nklcb/cosmos/member/entity/Member.java
+++ Back-end/cosmos/src/main/java/com/nklcb/cosmos/member/entity/Member.java
@@ -4,11 +4,15 @@ import com.nklcb.cosmos.global.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Member extends BaseEntity {
+public class Member extends BaseEntity implements UserDetails {
     @Column(length = 255, nullable = false)
     private String memberId;
 
@@ -17,4 +21,39 @@ public class Member extends BaseEntity {
 
     @Column(length = 255, nullable = false)
     private String name;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
