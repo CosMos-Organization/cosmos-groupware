619ffc46e5b83bad28b7430b254118bebd5cce9a
leeminjae <dlalswo9801@naver.com>
2024-03-21 00:38:31 +0900

    feat : @Valid 에러처리 add

--- Back-end/cosmos/src/main/java/com/nklcb/cosmos/global/advice/GlobalErrorAdvice.java
+++ Back-end/cosmos/src/main/java/com/nklcb/cosmos/global/advice/GlobalErrorAdvice.java
@@ -1,15 +1,36 @@
 package com.nklcb.cosmos.global.advice;
 
+import com.nklcb.cosmos.global.exception.NotFoundResultException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalErrorAdvice {
-
+    @ExceptionHandler(NotFoundResultException.class)
     public ResponseEntity<ErrorResponse> exception(final Exception e){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> processValidationError(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        StringBuilder errMessage = new StringBuilder();
+
+        for (FieldError error : result.getFieldErrors()) {
+            errMessage.append("[")
+                    .append(error.getField())
+                    .append("] ")
+                    .append(":")
+                    .append(error.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errMessage , HttpStatus.BAD_REQUEST);
     }
 
 }
